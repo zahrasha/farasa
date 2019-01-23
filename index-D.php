0710da23b07029ec94358fece507aaab327776c8
saeed <saeedshahdadd@gmail.com>
2019-01-23 22:32:36 +0330

    new page=index-D

---
+++ index-D.php
@@ -0,0 +1,26 @@
+<?php include 'head.php'; ?>
+</head>
+<body>
+<?php include 'header.php'; ?>
+    <!--start container-->
+    <div class="container">
+<h3>دوره های من</h3>
+<div>
+<a href="">
+<span>استانداردهای گزارش پژوهش های بالينی(CONSORT) </span> 
+<img src="images/50.jpg" alt="">
+</a>
+<span>موسسه فرزان	
+دسته: پژوهش پزشکی </span>
+<img src="images/ISBN-978-600-6135-00-7.jpg" alt="">
+</div>
+
+    </div>
+    <!--end container-->
+
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
+
+
