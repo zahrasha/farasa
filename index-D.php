2b1dd873cdfc61fe58fcb54b54730e6a0bbc34d8
saeed <saeedshahdadd@gmail.com>
2019-01-24 09:29:07 +0330

    create page profile and payment(اعتبار وپرداخت)

--- index-D.php
+++ index-D.php
@@ -4,23 +4,34 @@
 <?php include 'header.php'; ?>
     <!--start container-->
     <div class="container">
-<h5>دوره های من</h5>
-<div>
-<a href="">
-<span>استانداردهای گزارش پژوهش های بالينی(CONSORT) </span>
-<img src="images/50.jpg" alt="">
-</a> <span>موسسه فرزان	
-دسته: پژوهش پزشکی </span>
-
-<img src="images/ISBN-978-600-6135-00-7.jpg" alt="">
-</div>
-
-    </div>
+      <div class="row">
+        <div class="col-xs-3 col-md-3">
+           <h5 >دوره های من</h5>
+           <div class="card" style="width: 18rem;">
+            <img src="images/50.jpg"class="card-img-top" alt="..."> <img src="images/ISBN-978-600-6135-00-7.jpg" alt="">
+            <div class="card-body">
+              
+              <a href="">  
+              <span class="card-text">استانداردهای گزارش پژوهش های بالينی(CONSORT)</span>
+            </a> <br>
+            <span>
+                موسسه فرزان
+<br>
+                دسته: پژوهش پزشکی
+              </span></div>
+          </div>
+   
+      
+        </div>
+        </div>
+      </div>
     <!--end container-->
 
 <?php include 'footer.php'; ?>
 </body>
 
 </html>
+
+
 
 
---
+++ payment.php
@@ -0,0 +1,25 @@
+<?php include 'head.php'; ?>
+</head>
+<body>
+<?php include 'header.php'; ?>
+    <!--start container-->
+    <div class="container">
+<h5> اعتبار و پرداخت</h5>
+<table style="font-size:12px;color:#333333;width:50%;border-width: 1px;border-color: #729ea5;border-collapse: collapse;direction:rtl; text-align:center;">
+			<tr style="font-size:12px;background-color:#acc8cc;border-width: 1px;padding: 8px;border-style: solid;border-color: #729ea5;text-align:center;"><td>اعتبار فعال</td><td>میزان اعتبار</td></tr>
+			<tr><td>اعتبار اختصاص یافته به شما </td><td></td></tr>
+			<tr><td>اعتبار اختصاص یافته به حساب/مرکز شما</td><td></td></tr>
+			<!--<tr><td>بسته اعتباری خریداری شده </td><td></td></tr>-->
+						
+				
+			</table>
+
+
+    </div>
+    <!--end container-->
+
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
+
