6a4d6cfd294aeabf4d0f75210269c441190df809
Mohammad <zahrashah2019@gmail.com>
2019-01-25 17:57:49 +0330

    courses finished and course start

--- course.php
+++ course.php
@@ -4,7 +4,12 @@
 <?php include 'header.php'; ?>
     <!--start container-->
     <div class="container">
-
+    <div class="row margin-bottom-0">
+            <!--Top title-->
+            <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title "><i class="mdi-action-class"></i>استانداردهای گزارش پژوهش های بالينی(CONSORT)</h5>
+            </div>
+        </div>
 
 
     </div>
