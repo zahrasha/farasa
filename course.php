5bd4b942abfb0c6793fd18be5ad908b194e2fbd9
Mohammad <zahrashah2019@gmail.com>
2019-01-27 23:33:47 +0330

    content and course design

--- course.php
+++ course.php
@@ -11,103 +11,131 @@
             </div>
         </div>
         <div class="row">
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/forum.png" alt="">
-                    <h4 class="link">انجمن ها</h4>
-                  </a>
-                </div>
-            </div>
+            <div class="col s12 m4 l2 right">
+              <div class="white my-card z-depth-1 list-container">
+                <h4 class="heading">مرور مطالب</h4>
+                <ul>
+                <li><a href="course.php">صفحه اصلی دوره</a></li>
+                  <li><a href="content.php">پیش گفتار</a></li>
+                  <li>
+                  <a href="#">پیش گفتار</a>
+                    <ul>
+                      <li><a href="content.php">صفحه اصلی دوره</a></li>
+                      <li><a href="content.php">پیش گفتار</a></li>
+                      <li>
+                      <a href="#">پیش گفتار</a>
+                        <ul>
+                          <li><a href="content.php">صفحه اصلی دوره</a></li>
+                          <li><a href="content.php">پیش گفتار</a></li>
+                          <li><a href="content.php">پیش گفتار</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="content.php">لینک آخر</a></li>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/keywords.png" alt="">
-                    <h4 class="link">واژگان کلیدی</h4>
-                  </a>
-                </div>
+                </ul>
+              </div>
             </div>
+            <div class="col s12 m8 l10 right" id="MainLinks">
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/forum.png" alt="">
+                      <h4 class="link">انجمن ها</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/database.png" alt="">
-                    <h4 class="link">ذخیره سازی فایل</h4>
-                  </a>
-                </div>
-            </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/keywords.png" alt="">
+                      <h4 class="link">واژگان کلیدی</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/chat.png" alt="">
-                    <h4 class="link">گفتگوی آنلاین</h4>
-                  </a>
-                </div>
-            </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/database.png" alt="">
+                      <h4 class="link">ذخیره سازی فایل</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/link.png" alt="">
-                    <h4 class="link">لینک ها</h4>
-                  </a>
-                </div>
-            </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/chat.png" alt="">
+                      <h4 class="link">گفتگوی آنلاین</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/exam.png" alt="">
-                    <h4 class="link">آزمون ها</h4>
-                  </a>
-                </div>
-            </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/link.png" alt="">
+                      <h4 class="link">لینک ها</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/eye.png" alt="">
-                    <h4 class="link">بازدید ها</h4>
-                  </a>
-                </div>
-            </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/exam.png" alt="">
+                      <h4 class="link">آزمون ها</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/license.png" alt="">
-                    <h4 class="link">گواهی</h4>
-                  </a>
-                </div>
-            </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/eye.png" alt="">
+                      <h4 class="link">بازدید ها</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/meeting.png" alt="">
-                    <h4 class="link">نشستی با استاد</h4>
-                  </a>
-                </div>
-            </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/license.png" alt="">
+                      <h4 class="link">گواهی</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/payment.png" alt="">
-                    <h4 class="link">پرداخت و اعتبار</h4>
-                  </a>
-                </div>
-            </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/meeting.png" alt="">
+                      <h4 class="link">نشستی با استاد</h4>
+                    </a>
+                  </div>
+              </div>
 
-            <div class="col s6 m4 l2 right">
-                <div class="white center my-card bordered-card">
-                  <a href="#">
-                    <img src="./images/farasa/book.png" alt="">
-                    <h4 class="link">منابع آموزشی</h4>
-                  </a>
-                </div>
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/payment.png" alt="">
+                      <h4 class="link">پرداخت و اعتبار</h4>
+                    </a>
+                  </div>
+              </div>
+
+              <div class="col s6 m4 l2 right">
+                  <div class="white center my-card bordered-card">
+                    <a href="#">
+                      <img src="./images/farasa/book.png" alt="">
+                      <h4 class="link">منابع آموزشی</h4>
+                    </a>
+                  </div>
+              </div>
             </div>
 
         </div>
@@ -116,6 +144,12 @@
     <!--end container-->
 
 <?php include 'footer.php'; ?>
+
+<script>
+  $(".list-container ul li a").on("click",function(e){
+    $(this).siblings("ul").slideToggle();
+  })
+</script>
 </body>
 
 </html>
