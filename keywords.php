db25898616914e71f3fca4a5d5fb8dfd6aa0729a
Mohammad <zahrashah2019@gmail.com>
2019-02-09 11:20:58 +0330

    changing profile

--- keywords.php
+++ keywords.php
@@ -4,55 +4,86 @@
 <?php include 'header.php'; ?>
     <!--start container-->
     <div class="container">
-    <div class="row margin-bottom-0">
+        <div class="row margin-bottom-0">
             <!--Top title-->
             <div class="col s12 m12 l12">
-              <h5 class="breadcrumbs-title "><i class="mdi-action-class"></i>واژگان کلیدی</h5>
+                <h5 class="breadcrumbs-title "><i class="mdi-action-class"></i>واژگان کلیدی</h5>
             </div>
         </div>
-        <div class="container">
         <div class="row">
-        <div class="col s12">
-         <label for="">آنالیز تطبیق داده شده
-            Adjusted analysis</label>
-         <p>معمولاً به تلاش برای کنترل (تطبیق دادن) عدم تعادل در خصوصیات پایه بین گروه ها گفته می شود
-         . گاهی به تطبیق مقدار P برای ماسبه آزمون های چند گانه گفته می شود. مقایسه های چند گان
-          ه (multiple comparisons) را ببینید.</p>
-      </div> 
-      <div class="container">
-        <div class="row">
-        <div class="col s12">
-         <label for="">آنالیز تطبیق داده شده
-            Adjusted analysis</label>
-         <p>معمولاً به تلاش برای کنترل (تطبیق دادن) عدم تعادل در خصوصیات پایه بین گروه ها گفته می شود
-         . گاهی به تطبیق مقدار P برای ماسبه آزمون های چند گانه گفته می شود. مقایسه های چند گان
-          ه (multiple comparisons) را ببینید.</p>
-      </div>
-      <div class="container">
-        <div class="row">
-        <div class="col s12">
-         <label for="">آنالیز تطبیق داده شده
-            Adjusted analysis</label>
-         <p>معمولاً به تلاش برای کنترل (تطبیق دادن) عدم تعادل در خصوصیات پایه بین گروه ها گفته می شود
-         . گاهی به تطبیق مقدار P برای ماسبه آزمون های چند گانه گفته می شود. مقایسه های چند گان
-          ه (multiple comparisons) را ببینید.</p>
-      </div>
-      
-      <div class="pagination center   ">
-      <ul>
-      <li><a class=" waves-effect" href="#"><i>.</i></a></li>
-      <li><a class=" waves-effect" href="#">1</a></li>
-      <li><a class="waves-effect" href="#">2</a></li>
-      <li><a class="waves-effect" href="#">3</a></li>
-      <li><a class="waves-effect" href="#"><i>.</i></a></li>
-      </ul>
-      </div>
-    </div>
-    </div>
+            <div class="col s12 m4 l2 right">
+              <div class="white my-card z-depth-1 list-container">
+                <h4 class="heading">مرور مطالب</h4>
+                <ul>
+                <li><a href="course.php">صفحه اصلی دوره</a></li>
+                  <li><a href="content.php">پیش گفتار</a></li>
+                  <li>
+                    <a href="#">پیش گفتار</a>
+                    <ul>
+                      <li><a href="content.php">صفحه اصلی دوره</a></li>
+                      <li><a href="content.php">پیش گفتار</a></li>
+                      <li>
+                        <a href="#">پیش گفتار</a>
+                        <ul>
+                          <li><a href="content.php">صفحه اصلی دوره</a></li>
+                          <li><a href="content.php">پیش گفتار</a></li>
+                          <li><a href="content.php">پیش گفتار</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="content.php">لینک آخر</a></li>
+
+                </ul>
+              </div>
+            </div>
+            <div class="col s12 m8 l10 right" id="MainLinks">
+              <div class="white my-card bordered-card content-wrapper" style="padding: 15px;">
+                  <h3 class="main-title right">واژگان کلیدی</h3>
+                  <div class="clear"></div>
+                  <h5 class="keyword-title">تعيين اهداف شخصي:</h5>
+                  <p>
+                  روش مهمي است به منظور تعيين اينكه دستيابي به چه چيزي در زندگي براي شما مهم است.تفاوت قائل شدن بين آنچه كه مهم است، از آنچه كه بي‌ربط است يا موجب انحراف از اهداف زندگي مي‌شود و ايجاد انگيزه در خودتان افزايش اعتماد به نفس براساس دستيابي موفقيت‌آميز به اهداف.
+                  </p>
+                  <h5 class="keyword-title">اهداف بلندمدت:</h5>
+                  <p>
+                  با دادن دیدی جامع، به ساير جنبه‌های تصميم‌گيري‌ هر فرد شكل مي‌دهد.بهتر است مطمئن شد كه اهداف همان‌هايي هستند كه حقيقتاً خود فرد مي‌خواهد به آنها دست يابد، نه آنهايي كه پدر و مادر، خانواده يا كارفرما مي‌خواهند.
+                  </p>
+                  <h5 class="keyword-title">تئوری لاک:</h5>
+                  <p>
+                  دكتر ادوين لاك در زمينه تعيين اهداف و انگيزه در اواخر دهه 1960 بيان داشت كه كاركنان با داشتن اهداف روشن و دريافت بازخوردهاي مناسب انگيزه مي‌گیرند. لاك تا حدي پيش رفت كه گفت كار كردن براي يك هدف مشخص، انگيزه زيادي براي به واقعيت در آوردن آن هدف ايجاد مي‌كند كه به نوبه خود باعث بهبود عملكرد مي‌شود. يك راه مفيد براي قوي كردن اهداف اين است كه از كلمه SMART براي يادآوري استفاده كنيد.
+                  </p>
+                  <h5 class="keyword-title">عوامل اتلاف وقت:</h5>
+                  <p>
+                  شامل عوامل متعددی است مشتمل بر این موارد: مشخص نبودن هدف، عدم اولویت بندی کارها، وقفه های موجود و تعلل در کار.
+                  </p>
+                  <h5 class="keyword-title">اهرم‌هاي موفقيت:</h5>
+                  <p>
+                  استفاده از اهرم‌ها در واقع هنر و دانش انجام کارهاي بيشتر با استفاده از همان تلاش قبلي و يا کمتر است. در مورد اهداف ساده، اين کار زمان بيشتري را در اختيار افراد قرار مي‌دهد تا بر روي کارهايي که بيشترين اولويت را دارند تمرکز کند. در اهداف پيچيده‌تر، اين کار به افراد کمک مي‌کند تا به اهداف سطح بالاتري دست يافت.
+                  </p>
+                  <h5 class="keyword-title">اهرم زمان:</h5>
+                  <p>
+                  استفاده از اهرم زمان يکي از اساسي‌ترين استراتژي‌ها براي موفقيت است. در طول يک روز فقط ميزان مشخصي زمان وجود دارد که مي‌توانيد از آن استفاده کرد. میتوان با استفاده از وقت ديگران به عنوان اهرم، بهره‌وري را به ميزان فوق‌العاده‌اي افزايش داد.
+                  </p>
+                  <h5 class="keyword-title">اهرم توانايي‌ها:</h5>
+                  <p>
+                  افراد طيف وسيعي از مهارت‌ها، استعدادها، تجارب، افکار و عقايد را در اختيار دارند. اين‌ها مي‌توانند و بايد با بهترين ترکيب در کنار هم مورد استفاده قرار گيرند. يک روش خوب براي جواب دادن به اين سؤالات انجام آناليز SWOT است که سعي مي‌کند توانايي‌هاي فرد را شناسايي کند،ضمن اين که به وی کمک مي‌کند تا نقاط ضعف خود را که بايد برطرف کند،هرچه بهتر بشناسد.
+                  </p>
+              </div> 
+            </div>
+
+        </div>
+
     </div>
     <!--end container-->
 
 <?php include 'footer.php'; ?>
+
+<script>
+  $(".list-container ul li a").on("click",function(e){
+    $(this).siblings("ul").slideToggle();
+  })
+</script>
 </body>
 
 </html>
--- profile.php
+++ profile.php
@@ -1,130 +1,120 @@
-<?php include 'head.php'; ?>
+<?php include 'head.php';?>
 </head>
 <body>
-<?php include 'header.php'; ?>
+<?php include 'header.php';?>
     <!--start container-->
     <div class="container">
-	<div class="row margin-bottom-0">
+        <div class="row margin-bottom-0">
             <!--Top title-->
             <div class="col s12 m12 l12">
-                <h5 class="breadcrumbs-title "><i class="mdi-action-home"></i>  
-				   اطلاعات مورد نياز    </h5>
+                <h5 class="breadcrumbs-title "><i class="mdi-action-assignment-ind"></i>مشخصات كاربر</h5>
             </div>
         </div>
-    <form  method="post" action="/elearning2/users/profile.php" name="form" id="contactform">
+        <div class="row">
+            <div class="col s12 m12 l8 offset-l2" id="MainLinks">
+					<div class=" card z-depth-1 card-content white" style="padding: 15px;">
+							<form action="#">
+                            <div class="col s12">
+								<h4 class="heading">اطلاعات مورد نياز</h4>
+							</div>
+						    <div class="col s12 m6 l4 right">
+                                <div class="input-field ">
+                                    <i class="mdi-social-person prefix"></i>
+                                <input id="username" type="text" class="validate" value="09361503553" disabled >
+                                <label for="username" class="">نام كاربري</label>
+                            </div>
+							</div>
+							<div class="col s12 m6 l4 right ">
+                  		            <div class="input-field emailsection">
+                      		            <i class="mdi-communication-email prefix"></i>
+                                        <input id="email" type="text" class="validate" value="user7853132148@farasa.net">
+										<label for="email" class="">آدرس پست الكترونيك</label>
+										<!-- <span class="helper-text" data-error="wrong" data-success="right">Helper text</span> -->
+										<div class="right mycheckbox">
+                                        <input type="checkbox" id="barber">
+                                        <label for="barber" class="checkbox-text margin-bottom-0">پست الکترونيک را از ديگران مخفي کن</label>
+                        	            </div>
+							        </div>
+							</div>
+									<div class="input-field col s12 m6 l4 right mycheckbox">
+                                        <input type="checkbox" id="no-email">
+                                        <label for="no-email" class="checkbox-text margin-bottom-0">ایمیل ندارم</label>
+                                    </div>
+							<div class="clear margin-10"></div>
 
-	
-	  
-      <div class="row"> 
-	  
-	  <div class="input-field col s6 offset-s6">
-          <i class="material-icons prefix"></i>
-          <input type="text" id="autocomplete-input"  class="autocomplete">
-          <label for="autocomplete-input">نام كاربری</label>
+												<div class="col s12 m6 l4 right">
+                            <div class="input-field ">
+                                <i class="mdi-action-account-box prefix"></i>
+                                <input id="firstname" type="text" class="validate farsi" value="شیما">
+                                <label for="firstname" class="">نام</label>
+                            </div>
+												</div>
+                        <div class="col s12 m6 l4 right">
+                            <div class="input-field ">
+                                <i class="mdi-action-account-box prefix"></i>
+                                <input id="lastname" type="text" class="validate english" value="ابن العلم">
+                                <label for="lastname" class="">نام خانوادگی مسئول باشگاه</label>
+                            </div>
+                        </div>
+                        <div class="col s12 m6 l1 right">
+                            <small class="black-text">جنسیت</small>
+                        </div>
+                        <div class="input-field col s6 m4 l1 right myradio">
+                            <input type="radio" name="gender" id="men">
+                            <label for="men" class="checkbox-text black-text" style="font-size: 12pt">
+                              <span>مرد</span>
+                            </label>
+                        </div>
+                        <div class="input-field col s6 m4 l1 right myradio">
+                            <input type="radio" name="gender" id="women" checked> 
+                            <label for="women" class="checkbox-text black-text" style="font-size: 12pt">
+                              <span>زن</span>
+                            </label>
+                        </div>
+                        <div class="clear"></div>
+                        <div class="col s12 m6 l4 right">
+                            <div class="input-field farzan-select">
+                                <i class="mdi-action-credit-card prefix "></i>
+                                <label> حساب</label>
+                                <select title="استان">
+                                    <option value="1">گزینه 1</option>
+                                    <option value="2">گزینه 2</option>
+                                    <option value="3">گزینه 3</option>
+                                </select>
+                            </div>
+                        </div>
+                            <div class="clear"></div>
+                            <button class="btn waves-effect waves-light create-btn z-depth-1 light-blue accent-3 left submit-form" type="button">
+                                ذخیره
+                            </button>
+                            <button class="btn waves-effect waves-light create-btn z-depth-1 grey darken-2 left" type="button">
+
+                                انصراف
+                            </button>
+							<div class="clear"></div>
+
+                            </form>
+						</div>
+          </div>
+
         </div>
-      </div>
-	  
-	  <div class="row">
-	  <div class="input-field col s6 offset-s6">
-          <i class="material-icons prefix"></i>
-          <input type="text" id="autocomplete-input"  class="autocomplete">
-          <label for="autocomplete-input">آدرس پست الکترونیک</label>
-        </div>
-      </div>
 
-	  <div class="row">
-	  <div class="input-field col s6 offset-s6">
-          <i class="material-icons prefix"></i>
-          <input type="text" id="autocomplete-input"  class="autocomplete">
-          <label for="autocomplete-input">نام</label>
-        </div>
-      </div>
-
-	  <div class="row">
-	  <div class="input-field col s6 offset-s6">
-          <i class="material-icons prefix"></i>
-          <input type="text" id="autocomplete-input"  class="autocomplete">
-          <label for="autocomplete-input">نام خانوادگی</label>
-        </div>
-      </div>
-
-	  <div class="row">
-	  <div class="input-field col s6 offset-s6">
-          <i class="material-icons prefix"></i>
-          <select name="gender"  >
-				<option  value="male">مرد</option>
-				<option value="female">زن</option>
-				</select>
-          <label for="autocomplete-input">جنسیت</label>
-        </div>
-      </div>
-	  <div class="row">
-	  <div class="input-field col s6  offset-s6">
-          <i class="material-icons prefix"></i>
-          <select name="gender"  >
-				<option  value="male">فراسا</option>
-				</select>
-          <label for="autocomplete-input">حساب</label>
-        </div>
-      </div> 
-
-	  <div class="row">
-	  <div class="col s6 offset-s6">
-	  <div class="input-field ">
-	  <i class="material-icons prefix"></i><a href="#">
-	  <input class="btn blue col s3" type="submit" value="cancel" name="" id=""></a>
-	  <i class="material-icons prefix"></i> <a href="#">
-	  <input class="btn blue col s3" type="submit"  value="enter"name="" id=""></a>
-	  </div>
-      </div>
-      </div>
-	  </div>
-	<br>
-	<br>
-	<br>	
-	<br>
-	<br>	
-	<br>
-	  <div class="row" >
-	  <div class="col s6 offset-s6" >
-
-	            <br><label for="login"> نام کاربری</label><br />
-				<br><input class="autocomplete" name="member_id" type="text"  placeholder="username">
-				<br><label for="login">آدرس پست الكترونيك</label><br />
-				<br><input name="member_id" type="text"  placeholder="email">
-				<br><label for="login">نام</label><br />
-				<br><input name="member_id" type="text"  placeholder="name">
-				<br><label for="login"> نام خانوادگي</label><br />
-				<br><input name="member_id" type="text"  placeholder="family">
-				<br><label for="login">جنسیت</label><br />
-				<br>
-				<select name="gender"  >
-				<option  value="male">مرد</option>
-				<option value="female">زن</option>
-				</select>
-				<br><label for="login">حساب</label><br />
-				<select name="#"  >
-				<option  value="farasa">فراسا</option>
-				</select>
-				</div>
-				</div>
-				<div class="row">
-	  <div class="col s6 offset-s6">
-	  <div class="input-field ">
-	  <i class="material-icons prefix"></i><a href="#">
-	  <input class="btn blue col s3" type="submit" value="cancel" name="" id=""></a>
-	  <i class="material-icons prefix"></i> <a href="#">
-	  <input class="btn blue col s3" type="submit"  value="enter"name="" id=""></a>
-	  </div>
-		</div>
-		</div>
-
-		
-	
+    </div>
     <!--end container-->
 
-<?php include 'footer.php'; ?>
+<?php include 'footer.php';?>
+
+<script>
+  $(".list-container ul li a").on("click",function(e){
+    $(this).siblings("ul").slideToggle();
+  })
+    $("#no-email").on("change",function(){
+        $(".emailsection").toggle()
+    })
+</script>
 </body>
 
 </html>
+
+
 
