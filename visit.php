dd1482e3054576647eac0f32fd9bcc51b6c18037
saeed <saeedshahdadd@gmail.com>
2019-02-15 18:33:51 +0330

    add page visit.php

---
+++ visit.php
@@ -0,0 +1,90 @@
+<?php include 'head.php'; ?>
+
+<style>
+
+.table thead {
+  background:#0e92ef;
+  color:#ebf8f9;
+} 
+.membership-icon{
+    color: #0e92ef;
+    font-size: 21px;
+    position: relative;
+    top: 4px;
+    cursor: pointer;
+}
+
+</style>
+</head>
+<body>
+<?php include 'header.php'; ?>
+    <!--start container-->
+    <div class="container">
+        <div class="row margin-bottom-0">
+            <!--Top title-->
+            <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title "><i class="mdi-action-home"></i>
+Views</h5>
+            </div>
+        </div>
+        </div>
+   
+        <div class="container">
+        <div class="row">
+    <div class="col l12 right">
+    <table class="table responsive-table striped centered farzan-table z-depth-1">
+    <thead class="white-text ">
+    <tr class="my-table">
+        <th>Page</th>
+        <th>Visits</th>
+        <th>Total duration</th>
+        <th>Last access</th>
+       
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td><a href="#">Introduction</a></td>
+        <td >
+        3
+        </td>
+        <td>	00:09:42 </td>
+        <td> 	Tuesday 9 February, 06:44 </td>
+    </tr> 
+    <tr>
+        <td><a href="#">Introduction</a></td>
+        <td >
+        3
+        </td>
+        <td>	00:09:42 </td>
+        <td> 	Tuesday 9 February, 06:44 </td>
+    </tr>
+     <tr>
+        <td><a href="#">Introduction</a></td>
+        <td >
+        3
+        </td>
+        <td>	00:09:42 </td>
+        <td> 	Tuesday 9 February, 06:44 </td>
+    </tr>
+    
+    </tbody>
+    </table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+    <!--end container-->
+
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
+
+
+
