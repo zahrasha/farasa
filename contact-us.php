5bcf19e8c5a4fb26beb1881cf597e280fb7afeeb
saeed <saeedshahdadd@gmail.com>
2019-02-23 01:26:28 +0330

    .

---
+++ contact-us.php
@@ -0,0 +1,63 @@
+<?php include 'head.php'; ?>
+</head>
+<body>
+<?php include 'header.php'; ?>
+    <!--start container-->
+    <div class="container">
+        <div class="row margin-bottom-0">
+            <!--Top title-->
+            <div class="col s12 m12 l12">
+                <h5 class="breadcrumbs-title "><i class="mdi-action-home"></i>تماس با پشتيباني</h5>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col l12 m12 s12">
+            <div class="col s12 right">
+                <div class=" card z-depth-1  card-content col s10 l10 offset-l1 white">
+                    <form action="#">
+        <div class="col s12 m6 l3 right">
+            <div class="input-field ">
+                <i class="mdi-social-person prefix"></i>
+                <input id="username-cu" type="text" class="validate">
+                <label for="username-cu" class="">نام و نام خانوادگی</label>
+            </div>
+        </div>
+        <div class="col s12 m6 l3 right">
+            <div class=" input-field ">
+                <i class="mdi-content-mail prefix"></i>
+                <input id="username-az" type="text" class="validate">
+                <label for="username-az" class="">آدرس پست الكترونيكي</label>
+            </div>
+        </div>
+        <div class="col s12 m6 l3 right">
+            <div class="input-field ">
+                <i class=" prefix"></i>
+                <input id="username-sw" type="text" class="validate">
+                <label for="username-sw" class="">موضوع</label>
+            </div>
+        </div>
+        <div class="col s12 m6 l3 right">
+            <div class="input-field ">
+                <i class=" prefix"></i>
+                <input id="username-vc" type="textarea" class="validate">
+                <label for="username-vc" class="">بدنه</label>
+            </div>
+        </div>
+        <button class="btn waves-effect waves-light create-btn z-depth-1 light-blue accent-3 left submit-form"
+          type="button">جستجو
+        </button>
+        <div class="clear"></div>
+        </form>
+        </div>     
+        </div>     
+        </div>     
+        </div>
+        
+        </div>
+    </div>
+    <!--end container-->
+
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
