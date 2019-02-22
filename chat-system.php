1ce79db5daf8e3c0e4a2d0e7ad4934dd5eb3eb67
Mohammad <zahrashah2019@gmail.com>
2019-02-22 22:32:54 +0330

    chat sys

---
+++ chat-system.php
@@ -0,0 +1,118 @@
+<?php include 'head.php'; ?>
+<link href="./css/chat.css" type="text/css" rel="stylesheet" media="screen,projection">
+</head>
+<body>
+<?php include 'header.php'; ?>
+    <!--start container-->
+    <div class="container">
+        <div class="row margin-bottom-0">
+            <!--Top title-->
+            <div class="col s12 m12 l12">
+            <h5 class="breadcrumbs-title "><i class="mdi-action-home"></i>صفحه اصلی /گفتگوی انلاین/پیام ها</h5>
+            </div>
+        </div>
+        <div class="row">
+          <div class="col s12 m4 l3 right">
+            <div class=" card main-info padding-5" style="padding-right:15px">
+              <p><b>انتخاب</b></p>
+              <span>
+                <a href="#">ویرایش علایق</a> | 
+                <a href="#">خروج از گفتگوی آنلاین</a>
+              </span>
+              <div class="clear"></div>
+
+              <p><b>ليست کاربران و گزارش</b></p>
+              <p> 09361503553 (شما)</p> 
+              <span>
+                <a href="#">گزارش کامل</a> | 
+                <a href="#">بازبيني ليست کاربران</a>
+              </span>
+              <div class="clear"></div>
+              <p><b>كليدهاي ميانبر</b></p>
+              <p>
+              Alt + C: جهش به ايجاد پيام<br>
+              Enter: ارسال پِيام کامل شده<br>
+              Alt + R : بازبيني پيامها<br>
+              Alt + M : جهش به پيامها<br>
+              Alt + Q : خروج از گفتگوي آنلاين<br>
+              </p>
+            </div>
+          </div>
+          <div class="col s12 m8 l9">
+            <div class="card main-chat">
+            <div id="frame">
+              <div class="content">
+                <div class="messages">
+                  <ul>
+                    <li class="sent">
+                      <img src="http://emilcarlsson.se/assets/mikeross.png" alt="" />
+                      <p>
+                      لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.
+                      </p>
+                    </li>
+                    <li class="replies">
+                      <img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+                      <p>
+                      ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنا
+                      </p>
+                    </li>
+                    <li class="replies">
+                      <img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+                      <p>ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها </p>
+                    </li>
+                    <li class="sent">
+                      <img src="http://emilcarlsson.se/assets/mikeross.png" alt="" />
+                      <p>ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنا</p>
+                    </li>
+                    <li class="replies">
+                      <img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+                      <p>
+                      ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                      </p>
+                    </li>
+                    <li class="replies">
+                      <img src="http://emilcarlsson.se/assets/harveyspecter.png" alt="" />
+                      <p>
+                      ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                      </p>
+                    </li>
+                    <li class="sent">
+                      <img src="http://emilcarlsson.se/assets/mikeross.png" alt="" />
+                      <p>
+                      ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.                      
+                      </p>
+                    </li>
+                    <li class="sent alert">
+                      <p>
+                        کاربر از سیستم خارج شد.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+                <div class="message-input">
+                  <div class="wrap">
+                  <input type="text" placeholder="پیام خود را بنویسید..." />
+                  <i class="fa fa-paperclip attachment" aria-hidden="true"></i>
+                  <button class="submit"><i class="fa fa-paper-plane" aria-hidden="true"></i></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            </div>
+          </div>
+        </div>
+
+    </div>
+    <!--end container-->
+
+<?php include 'footer.php'; ?>
+</body>
+
+</html>
+
+
+
+
+
+
